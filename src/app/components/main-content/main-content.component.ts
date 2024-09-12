85118cf30b0fd0cb4f1fb82a00eac06e341d7e47
Harr-izle <harrietbuadee@gmail.com>
2024-09-12 13:50:43 +0000

    designs footer

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FruitsLayoutComponent } from "../fruits-layout/fruits-layout.component";
+import { VeggiesLayoutComponent } from "../veggies-layout/veggies-layout.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [FruitsLayoutComponent],
+  imports: [FruitsLayoutComponent, VeggiesLayoutComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
