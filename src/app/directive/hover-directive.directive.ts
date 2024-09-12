3b4e33c2f92583f18d087300292d24f0ffde7796
Harr-izle <harrietbuadee@gmail.com>
2024-09-12 13:58:21 +0000

    generates directive for hover effect

---
+++ src/app/directive/hover-directive.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appHoverDirective]',
+  standalone: true
+})
+export class HoverDirectiveDirective {
+
+  constructor() { }
+
+}
